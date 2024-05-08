901e16734e87187944173140a017dbb3cb5bc295
Timo Ley <timo.ley@tilera.dev>
2024-05-08 11:50:58 +0200

    feat: implement observer

--- assets/scripts/modules/observer.js
+++ assets/scripts/modules/observer.js
@@ -2,7 +2,20 @@
 ---------------------------------------------------------------------------- */
 
 const addObserver = () => {
-
+    const observedElements = document.querySelectorAll("[data-js-observe]");
+    const options = {rootMargin: "0px", threshold: 0.6};
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("is-in-view");
+            } else {
+                entry.target.classList.remove("is-in-view");
+            }
+        });
+    }, options);
+    observedElements.forEach((element) => {
+        observer.observe(element);
+    });
 }
 
 
